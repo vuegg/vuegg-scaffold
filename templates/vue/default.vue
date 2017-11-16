6e2b15ea4f7baf64b9c34e0479881776c52f91c2
alxpez <alexperezdev@gmail.com>
2017-11-16 22:12:30 +1300

    change template

--- templates/vue/default.vue
+++ templates/vue/default.vue
@@ -4,13 +4,11 @@
 </template>
 
 <script>{{COMPONENTS_IMPORTS}}
-
 export default {
   name: '{{VUEGG_NAME}}'{{COMPONENTS_DECLARATIONS}}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-{{VUEGG_STYLES}}
+<style scoped>{{VUEGG_STYLES}}
 </style>
