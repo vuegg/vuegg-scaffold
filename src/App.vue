c78bbc3e83c2fa10911e69c61a90d24185e96ec6
alxpez <alexperezdev@gmail.com>
2018-01-23 20:39:42 +1300

    minor fix related to vuegg/issues/#42

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,11 @@ export default {
 </script>
 
 <style>
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+
 #app {
   font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
